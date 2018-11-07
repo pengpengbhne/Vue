720bb82165d6e0a68b9f83c60cb1a1ec7bb740ac
pengpengbhne <44281830+pengpengbhne@users.noreply.github.com>
2018-11-07 09:21:49 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<!--html结构-->
+<template>
+  <div id="app">
+    hello vue.js
+    <h1>{{title}}</h1>
+    <users></users>
+  </div>
+</template>
+<!--行为-->
+<script>
+//局部注册组件
+import Users from './components/Users'
+export default {
+  name: 'App',
+  data(){
+    return {
+      title:'这个一个脚手架项目'
+    }
+  },
+  components:{
+    "users":Users
+  }
+}
+</script>
+<!--样式-->
+<style>
+
+</style>
---
+++ src/components/Users.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="users">
+    <ul>
+        <li v-for="user in users">
+        {{user}}
+        </li>
+    </ul>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name:'users',
+  data(){
+      return {
+          users:['lisi','zhangsan','wangwu']
+      }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
